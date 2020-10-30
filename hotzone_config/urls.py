d39963b9cc1c9111ae9a168eb19a29f19e00ad97
mesbaulalam <mesbaul.a.khan@gmail.com>
2020-10-30 19:25:32 +0800

    Created Location Search API

--- hotzone_config/urls.py
+++ hotzone_config/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include   
+from locations.views import location_search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('locations/', include("locations.urls")),
+    path('', location_search),
 ]
--- locations/views.py
+++ locations/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
+import requests
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello World. youre at the locations index")
+def location_search(request):
+    output = ""
+    if request.method == "POST":
+        location = request.POST.get('title')
+        PARAMS = {'q':location}
+        URL = "https://geodata.gov.hk/gs/api/v1.0.0/locationSearch"
+        response = requests.get(
+            url=URL,params=PARAMS).json()
+        output = response[0]["nameEN"]
+    context = {
+        "info": output
+    }
+    return render(request, "locations.html", context)
